d9268983e562002c8d31d2dc82cc8e6423e6d92e
TakemiKazuchi <thuyvanthai2013@gmail.com>
2023-05-09 15:00:14 +0700

    Revert "sdasdsdafdf"
    This reverts commit 68a70e42

--- demo/src/main/java/com/example/demo/StatService.java
+++ demo/src/main/java/com/example/demo/StatService.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.RetryableTopic;
-import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,15 +13,9 @@ public class StatService {
     @Autowired
     private StatRepository statRepository;
 
-
-//    @RetryableTopic(attempts = "3",dltTopicSuffix = "-dlt",backoff = @Backoff(delay = 2_000,multiplier = 3))
-    @KafkaListener(id="TXTHAIstatService",topics = "stat")
+    @KafkaListener(id="statService",topics = "stat")
     public void listen(StatDTO statDTO){
         logger.info("received: "+statDTO.getMessage());
         statRepository.save(statDTO);
     }
-//    @KafkaListener(id="dltTXTHAIGroup",topics = "stat-dlt")
-//    public void dltRetryableGroupListener(StatDTO statDTO){
-//        logger.info("received on Statistic TXTHAI DLT Topics: " + statDTO.getMessage());
-//    }
 }
--- kafka-consumer/src/main/java/com/example/kafkaconsumer/KafkaConsumerApplication.java
+++ kafka-consumer/src/main/java/com/example/kafkaconsumer/KafkaConsumerApplication.java
@@ -1,12 +1,17 @@
 package com.example.kafkaconsumer;
 
+import com.example.kafkaconsumer.model.MessageDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.converter.JsonMessageConverter;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.util.backoff.FixedBackOff;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.util.Properties;
@@ -32,5 +37,10 @@ public class KafkaConsumerApplication {
     @Bean
     SpringTemplateEngine springTemplateEngine(){
         return new SpringTemplateEngine();
+    }
+
+    @Bean
+    DefaultErrorHandler errorHandler(KafkaOperations<String,Object> template){
+        return new DefaultErrorHandler(new DeadLetterPublishingRecoverer(template),new FixedBackOff(1000L,2));
     }
 }
